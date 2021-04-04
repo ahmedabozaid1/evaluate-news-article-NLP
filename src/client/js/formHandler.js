4d913ef48515d4f44f88ea39f3a91ec17ebba39e
Ahmed Abozaid <73134650+ahmedabozaid1@users.noreply.github.com>
2021-04-04 16:32:21 +0200

    Add files via upload

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,47 +1,44 @@
 import { checkURL } from "./checkURL"
-const fetch = require("node-fetch");
+const fetch = require("node-fetch")
+
+const link="http://localhost:8081/check";
 async function handleSubmit(event) {
     event.preventDefault()
-    ///alert('here')
     let url = document.getElementById('url').value
-
     if(Client.checkURL(url))
-    {
-        ///alert(url
-        ///send to backend 
-        post("http://localhost:8081/check",{url: url}).then(data =>{
-            document.getElementById("agreement").innerHTML= `Agreement: ${data.agreement}`;
-            document.getElementById("subjectivity").innerHTML=`Subjectivity: ${data.subjectivity}`;
-            document.getElementById("confidence").innerHTML=`Confidence: ${data.confidence}`;
-            document.getElementById("irony").innerHTML=`Irony: ${data.irony}`;
-        })
-      
-       
+    { 
+        sendData(url);
+              
     }else{ 
         alert('URL is Wrong')
         console.log("wrong")
     }
-
 }
 
 
+function sendData(url)
+{
+    postData(link,{url: url}).then(
+        data =>{
+        document.getElementById("agreement").innerHTML= `Agreement: ${data.agreement}`;
+        document.getElementById("subjectivity").innerHTML=`Subjectivity: ${data.subjectivity}`;
+        document.getElementById("confidence").innerHTML=`Confidence: ${data.confidence}`;
+        document.getElementById("irony").innerHTML=`Irony: ${data.irony}`;
+    }) 
+}
 
-
-const post = async (url = '', data = {}) => {
+/* source: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch  (post method)*/
+async function postData(url = '', data = {}) {
     const response = await fetch(url, {
-        method: 'POST',
-        credentials: 'same-origin',
-        mode: 'cors',
+        method: 'POST', 
+        mode: 'cors', 
+        credentials: 'same-origin', 
         headers: {
-            'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(data)
-    })
-    try {
-        return await response.json()
-    } catch (error) {
-        console.log(error)
-    }
+    });
+    return response.json()
 }
 
 export { handleSubmit }
